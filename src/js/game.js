c898deefaed438a532715e9f1814e22f7f887cdb
Ricardo Avancini <ravancini@PWST215s-MacBook-Pro.local>
2021-05-02 23:31:12 +0100

    Measure cell neighbor bombs

--- src/js/game.js
+++ src/js/game.js
@@ -4,7 +4,8 @@
 		$columnsNumber = $('#columns-number'),
 		$area = $('.game-area'),
 		$loader = $('#loader'),
-		$start = $('#btn-start');
+		$start = $('#btn-start'),
+		$restart = $('#btn-restart');
 
 
 	const draw = () => {
@@ -15,9 +16,9 @@
 		$area.empty()
 		let html = ''
 
-		for (let row = 0; row < nr; row++) {
+		for (let row = 1; row < (nr+1); row++) {
 			html += `<div id="row-${row}" class="game-row">`
-			for (let col = 1; col < nc; col++) {
+			for (let col = 1; col < (nc+1); col++) {
 				html += `<div id="cell-${row}-${col}" class="game-cell" style="height: ${side}px; width: ${side}px;"></div>`
 			}
 			html += '</div>'
@@ -51,13 +52,23 @@
 			redraw($columnsNumber, parseInt($columnsNumber.val()) -1)
 		})
 		$start.on('click', () => startGame())
+		$restart.on('click', () => restartGame())
 	}
 
 	const startGame = () => {
 		$loader.show()
 		drawBombs()
 		addGameCellsHandler()
+		$start.hide()
+		$restart.show()
 		$loader.hide()
+	}
+
+	const restartGame = () => {
+		init()
+		$('#img-smiley-happy').hide()
+		$('#img-smiley-sad').show()
+		startGame()
 	}
 
 	const drawBombs = () => {
@@ -74,20 +85,61 @@
 				continue
 			}
 			$cell.data('bomb', true)
-			$cell.css('background-color', 'yellow')
 		}
 	}
 	
 	const addGameCellsHandler = () => {
 		$('.game-cell').on('click', (e) => {
-			if($(e.target).data('bomb') === true) {
-				// TODO - End Game
+			const $e = $(e.target)
+			if($e.data('bomb') === true) {
+				endGame($e)
 			} else {
-				// TODO - Open cells and calculate numbers
+				analizeCell($e)
 			}
 		})
 	}
 
-	draw()
-	addHandlers()
+	const analizeCell = ($e) => {
+		const neighboringCells = []
+		let [, row, col] = $e.attr('id').split('-')
+		row = parseInt(row)
+		col = parseInt(col)
+		let number = 0
+		for (let r = (row-1); r < (row + 2); r++) {
+			for (let c = (col-1); c < (col + 2); c++) {
+				const $cell = $(`#cell-${r}-${c}`)
+				if ((!$cell.length) || 
+				(row === r && col === c)) continue
+				if ($cell.data('bomb') === true) number++
+				if (!$cell.hasClass('empty') && !$cell.hasClass('number')) neighboringCells.push($cell)
+			}
+		}
+		
+		if (number !== 0) {
+			$e.css('background-image', `url('./src/img/${number}.svg')`)
+			$e.addClass('number')
+		} else if(!$e.hasClass('empty')) {
+			$e.addClass('empty')
+			neighboringCells.forEach($el => {
+				if(!$el.hasClass('empty')) {
+					analizeCell($el)
+				}
+			})
+		}		
+	}
+
+	const endGame = ($e) => {
+		$e.addClass('explosion')
+		$('#img-smiley-happy').hide()
+		$('#img-smiley-sad').show()
+		// TODO - Show all bombs and disable cells
+	}
+
+	const init = () => {
+		draw()
+		addHandlers()
+	}
+
+	init()
+	
 })()
